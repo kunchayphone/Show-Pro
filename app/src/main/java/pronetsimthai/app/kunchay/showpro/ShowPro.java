35897da8306566a9075a6495c8c9055ff6cd3758
kunchayphone <c_chay02@hotmail.com>
2016-11-09 16:48:00 +0700

    Intent to ShoPro finish

--- app/src/main/java/pronetsimthai/app/kunchay/showpro/ShowPro.java
+++ app/src/main/java/pronetsimthai/app/kunchay/showpro/ShowPro.java
@@ -2,12 +2,22 @@ package pronetsimthai.app.kunchay.showpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ShowPro extends AppCompatActivity {
+
+    // Exlicit
+    private int anInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_pro);
-    }
-}
+
+        //Get Value From Intent
+        anInt = getIntent().getIntExtra("Index", 1);
+        Log.d("9novV1", "Index ==> " + anInt);
+
+    } // Main Method
+
+} // Main Class
