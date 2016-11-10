28e063579787bcf2e6591a5d46e7f5519135712d
kunchayphone <c_chay02@hotmail.com>
2016-11-10 13:11:32 +0700

    Change Image Finish

--- app/src/main/java/pronetsimthai/app/kunchay/showpro/ShowPro.java
+++ app/src/main/java/pronetsimthai/app/kunchay/showpro/ShowPro.java
@@ -3,21 +3,53 @@ package pronetsimthai.app.kunchay.showpro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class ShowPro extends AppCompatActivity {
 
     // Exlicit
     private int anInt;
+    private ImageView imageView;
+    private ListView listView;
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_pro);
 
+        //Bind Widget
+        imageView = (ImageView) findViewById(R.id.imageView4);
+        listView = (ListView) findViewById(R.id.LivPromotion);
+        button = (Button) findViewById(R.id.button);
+
+
         //Get Value From Intent
         anInt = getIntent().getIntExtra("Index", 1);
         Log.d("9novV1", "Index ==> " + anInt);
 
+
+        //Back Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        //Show Image
+        int[] iconInts = new int[]{0,R.drawable.ais_logo,
+        R.drawable.dtac_logo,R.drawable.true_logo};
+        imageView.setImageResource(iconInts[anInt]);
+
+
     } // Main Method
+
+
 
 } // Main Class
